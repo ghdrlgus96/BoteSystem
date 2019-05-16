33559123146c8e88c3d9cc5cbe47e84a745abe32
홍기현 <ghdrlgus96@naver.com>
2019-05-16 17:09:34 +0900

    190516 홍기현 -> 아이디 찾기 구현

--- app/src/main/java/embedded/block/vote/FindidActivity.kt
+++ app/src/main/java/embedded/block/vote/FindidActivity.kt
@@ -1,8 +1,18 @@
 package embedded.block.vote
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.android.volley.AuthFailureError
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.activity_findid.*
+import org.json.JSONObject
+import java.util.HashMap
 
 class FindidActivity : AppCompatActivity() {
 
@@ -11,11 +21,34 @@ class FindidActivity : AppCompatActivity() {
         setContentView(R.layout.activity_findid)
 
         findBtn.setOnClickListener {
-            findId(findIDNameText.text.toString(), findIDPhone.text.toString())
+            findId(findIDNameText.text.toString(), findIDPhoneText.text.toString())
         }
     }
 
     fun findId(name: String?, phone: String?) {
+        var json = JSONObject()
+        json.put("myname", name)
+        json.put("myphone", phone)
 
+        var queue: RequestQueue = Volley.newRequestQueue(this);
+        val request = object : JsonObjectRequest(
+            Request.Method.POST, "http://203.249.127.32:65001/bote/find/id", json,
+            Response.Listener { response ->
+                run {
+                    if(response.getString("userid") == "undefind")
+                        Toast.makeText(this, "정확한 정보를 입력해주세요", Toast.LENGTH_SHORT).show()
+                    else
+                        Toast.makeText(this, response.getString("userid"), Toast.LENGTH_SHORT).show()
+                }
+            }, null
+        ) {
+            @Throws(AuthFailureError::class)
+            override fun getHeaders(): MutableMap<String, String>? {
+                val headers = HashMap<String, String>()
+                headers.put("Content-Type", "application/json")
+                return headers
+            }
+        }
+        queue.add(request)
     }
 }

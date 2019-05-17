74575cb9e2a494aedeab1128f09cc356e6dbf33b
Younjae <jhsl31@naver.com>
2019-05-17 18:49:58 +0900

    투표 등록 - 참여자 선택 최종

--- app/src/main/java/embedded/block/vote/AdminInputAdapter.kt
+++ app/src/main/java/embedded/block/vote/AdminInputAdapter.kt
@@ -9,11 +9,13 @@ import android.widget.BaseAdapter
 import android.widget.TextView
 import org.json.JSONArray
 
-class AdminInputAdapter(val context: Context): BaseAdapter() {
+class AdminInputAdapter(val context: Context) : BaseAdapter() {
     companion object {
         var arr_getParticipation = JSONArray()
     }
-    private val mInflater: LayoutInflater = LayoutInflater.from(context)
+
+    var temp = false
+    val mInflater: LayoutInflater = LayoutInflater.from(context)
     override fun getCount(): Int {
         Log.d("etest", arr_getParticipation.length().toString())
         return arr_getParticipation.length()
@@ -24,11 +26,18 @@ class AdminInputAdapter(val context: Context): BaseAdapter() {
     override fun getItemId(position: Int) = position.toLong()
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-
         val view = convertView ?: mInflater.inflate(R.layout.admin_input_select, parent, false)
-                    view.findViewById<TextView>(R.id.textView_admin_input_item1).text = getItem(position).getString("userName")
-                    view.findViewById<TextView>(R.id.textView_admin_input_item2).text = getItem(position).getString("userPhone")
-                    view.findViewById<TextView>(R.id.textView_admin_input_item3).text = getItem(position).getString("userClass")
+
+        if (temp == false) {
+            Log.d("etest", "새봄!")
+            view.findViewById<TextView>(R.id.textView_admin_input_item1).text =
+                getItem(position).getString("userName")
+            view.findViewById<TextView>(R.id.textView_admin_input_item2).text =
+                getItem(position).getString("userPhone")
+            view.findViewById<TextView>(R.id.textView_admin_input_item3).text =
+                getItem(position).getString("userClass")
+        }
         return view
     }
+
 }
